13cb79629058daec9ac55d7889663a5cd86c705c
Olav Stetter <olav.stetter@googlemail.com>
2015-10-11 15:15:04 +0200

    Switched to standard Rails caching.

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -12,7 +12,7 @@ class Developer
   scope :active, -> { where(:active.in => [nil, true]) }
 
   def self.senior_latest_starting_date
-    @@senior_latest_starting_date ||= begin
+    Rails.cache.fetch(:senior_latest_starting_date) do
       all_devs = self.active.order_by(starting_date: 'asc').to_a
       limit_index = (all_devs.count.to_f / 2).to_i
       newest_senior_dev = all_devs[limit_index]
@@ -21,7 +21,7 @@ class Developer
   end
 
   def self.reset_seniority_limit
-    @@senior_latest_starting_date = nil
+    Rails.cache.delete(:senior_latest_starting_date)
   end
 
 
