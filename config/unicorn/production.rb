608a3987d34a75026ba0ca872aa5018a71d23937
Urban Hafner <contact@urbanhafner.com>
2016-06-06 10:21:58 +0200

    Better unicorn configuration

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -9,6 +9,20 @@ stdout_path File.join(app_path, 'log', 'unicorn.log')
 pid File.join(app_path, 'tmp', 'pids', 'unicorn.pid')
 
 before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  old_pid = File.join(app_path, 'tmp', 'pids', 'unicorn.pid.oldbin')
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      puts 'Someone else did the job for us.'
+    end
+  end
+
   ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 end
 
