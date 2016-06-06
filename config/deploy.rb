06a8b058a0da3bb01b2e5c298f767cd2c5f68e25
Urban Hafner <contact@urbanhafner.com>
2016-06-06 13:38:07 +0200

    Secrets for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:riskmethods/Mentorship.git'
 set :deploy_to, '/home/riskmethods/Mentorship'
 set :asset_roles, [:web]
 set :unicorn_rack_env, 'production'
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('tmp', 'log', 'public/assets')
 
 after 'deploy:publishing', 'deploy:restart'
